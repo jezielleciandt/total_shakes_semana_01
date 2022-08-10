5f90c6ece2f585b79406c1e3099566423d0f86b7
Carol <anacs@ciandt.com>
2022-08-10 16:18:35 -0300

    Finished project

--- src/main/java/pedido/Pedido.java
+++ src/main/java/pedido/Pedido.java
@@ -1,49 +1,111 @@
 package pedido;
 
+import ingredientes.Adicional;
+import ingredientes.Ingrediente;
+
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Map;
 
-public class Pedido{
+public class Pedido {
 
-    private int id;
-    private  ArrayList<ItemPedido> itens;
-    private Cliente cliente;
+    private final int id;
+    private final ArrayList<ItemPedido> itens;
+    private final Cliente cliente;
 
-    public Pedido(int id, ArrayList<ItemPedido> itens,Cliente cliente){
+    public Pedido(int id, ArrayList<ItemPedido> itens, Cliente cliente) {
         this.id = id;
-        this.itens=itens;
-        this.cliente=cliente;
+        this.itens = itens;
+        this.cliente = cliente;
     }
 
     public ArrayList<ItemPedido> getItens() {
         return itens;
     }
 
-    public int getId(){
+    public int getId() {
         return this.id;
     }
 
-    public Cliente getCliente(){
+    public Cliente getCliente() {
         return this.cliente;
     }
 
-    public double calcularTotal(Cardapio cardapio){
-        double total= 0;
-        //TODO
+    public double calcularTotal(Cardapio cardapio) {
+        Double total = (double) 0;
+        for (ItemPedido itemPedido : this.itens) {
+            Double totalItem = (double) 0;
+            for (Map.Entry<Ingrediente, Double> precoIngrediente : cardapio.getPrecos().entrySet()) {
+                if (itemPedido.getShake().getBase().compareTo(precoIngrediente.getKey()) == 0) {
+                    totalItem += precoIngrediente.getValue();
+                }
+            }
+            totalItem = totalItem * itemPedido.getShake().getTipoTamanho().multiplicador;
+            for (Map.Entry<Ingrediente, Double> precoIngrediente : cardapio.getPrecos().entrySet()) {
+                for (Adicional adicional : itemPedido.getShake().getAdicionais()) {
+                    if (adicional.equals(precoIngrediente.getKey())) {
+                        totalItem += precoIngrediente.getValue();
+                    }
+                }
+            }
+            totalItem = totalItem * itemPedido.getQuantidade();
+            total += totalItem;
+        }
         return total;
     }
 
-    public void adicionarItemPedido(ItemPedido itemPedidoAdicionado){
-        //TODO
+    public void adicionarItemPedido(ItemPedido itemPedidoAdicionado) {
+        sortAdicionais(itemPedidoAdicionado);
+        boolean added = false;
+        for (ItemPedido item : itens) {
+            if (!added) {
+                if (item.getShake().equals(itemPedidoAdicionado.getShake())) {
+                    item.setQuantidade(item.getQuantidade() + itemPedidoAdicionado.getQuantidade());
+                    added = true;
+                }
+            }
+        }
+
+        if (!added) {
+            itens.add(itemPedidoAdicionado);
+        }
     }
 
     public boolean removeItemPedido(ItemPedido itemPedidoRemovido) {
-        //substitua o true por uma condição
-        if (true) {
-            //TODO
-        } else {
-            throw new IllegalArgumentException("Item nao existe no pedido.");
+        sortAdicionais(itemPedidoRemovido);
+        ItemPedido itemToRemove = null;
+        boolean removed = false;
+        for (ItemPedido item : itens) {
+            if (!removed) {
+                if (item.getShake().equals(itemPedidoRemovido.getShake())) {
+                    if (item.getQuantidade() == 1) {
+                        itemToRemove = item;
+                        removed = true;
+                    } else {
+                        item.setQuantidade(item.getQuantidade() - 1);
+                        removed = true;
+                    }
+                }
+            }
         }
+        if (itemToRemove != null)
+            itens.remove(itemToRemove);
+        if (!removed)
+            throw new IllegalArgumentException("Item nao existe no pedido.");
         return false;
+    }
+
+    public void sortAdicionais(ItemPedido pedido) {
+        if (pedido.getShake().getAdicionais() != null) {
+            if (pedido.getShake().getAdicionais().size() > 0) {
+                pedido.getShake().getAdicionais().sort(new Comparator<Ingrediente>() {
+                    @Override
+                    public int compare(Ingrediente o1, Ingrediente o2) {
+                        return o1.obterTipo().toString().compareTo(o2.obterTipo().toString());
+                    }
+                });
+            }
+        }
     }
 
     @Override
--- src/main/java/produto/Shake.java
+++ src/main/java/produto/Shake.java
@@ -1,6 +1,9 @@
 package produto;
 
-import ingredientes.*;
+import ingredientes.Adicional;
+import ingredientes.Base;
+import ingredientes.Fruta;
+import ingredientes.Topping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,8 +12,23 @@ public class Shake {
     private Base base;
     private Fruta fruta;
     private Topping topping;
-    private List<Adicional> adicionais;
-    private TipoTamanho  tipoTamanho;
+    private List<Adicional> adicionais = new ArrayList<>();
+    private TipoTamanho tipoTamanho;
+
+    public Shake(Base base, Fruta fruta, Topping topping, TipoTamanho tipoTamanho) {
+        this.base = base;
+        this.fruta = fruta;
+        this.topping = topping;
+        this.tipoTamanho = tipoTamanho;
+    }
+
+    public Shake(Base base, Fruta fruta, Topping topping, List<Adicional> adicionais, TipoTamanho tipoTamanho) {
+        this.base = base;
+        this.fruta = fruta;
+        this.topping = topping;
+        this.adicionais = adicionais;
+        this.tipoTamanho = tipoTamanho;
+    }
 
     public Base getBase() {
         return base;
@@ -35,5 +53,32 @@ public class Shake {
     @Override
     public String toString() {
         return this.base.getTipoBase().toString() + " / " + this.fruta.getTipoFruta().toString() + " / " + this.topping.getTipoTopping().toString() + " / " + this.adicionais + " / " + this.tipoTamanho.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (!(o instanceof Shake)) {
+            return false;
+        }
+
+        Shake c = (Shake) o;
+
+        if (c.getBase().toString().equals(this.getBase().toString())
+                && c.getFruta().toString().equals(this.getFruta().toString())
+                && c.getTopping().toString().equals(this.getTopping().toString())
+                && c.getTipoTamanho().toString().equals(this.getTipoTamanho().toString())) {
+            ArrayList<Adicional> cp = new ArrayList<>(this.getAdicionais());
+            for (Adicional ad : c.getAdicionais()) {
+                if (!cp.remove(ad)) {
+                    return false;
+                }
+            }
+            return cp.isEmpty();
+        }
+        return false;
     }
 }
