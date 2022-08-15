6144fce5b69edb9dea7d80b74d8adf1449debc8d
Eric <ericsousa@ciandt.com>
2022-08-15 10:53:27 -0300

    Branch desafio da semana_1

--- src/main/java/demo/Program.java
+++ src/main/java/demo/Program.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
+
         Cliente cliente = new Cliente(1,"Pedro","pedro@email.com");
         Cardapio cardapio = new Cardapio();
 
